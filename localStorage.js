e66e81627a0377a577ea4917333f65b3dbf10a4a
= <=>
2023-02-25 18:48:19 -0500

    Login/Registro 1ra version

--- localStorage.js
+++ localStorage.js
@@ -4,5 +4,85 @@ let correo;
 let img_perfil; 
 
 window.onload = function(){
-	frm_login = document.getElementById("frm_login")
+	frm_login = document.getElementById("frm_login");
+	btn_enviar = document.getElementById("btn_login");
+	btn_enviar_registrar = document.getElementById("btn_enviar_registrar");
+	frm_registro = document.getElementById("frm_register");
+
+	configurar_login();
+	configurar_registro();
 }
+
+function configurar_login(){
+	frm_login.addEventListener('submit', event => {
+		event.preventDefault();
+		event.stopPropagation();
+		if(frm_login.checkValidity()){
+			compararClave();
+		}
+		frm_login.classList.add('was-validated')
+	}, false);
+}
+
+function configurar_registro(){
+  frm_registro.addEventListener('submit', event => {
+      event.preventDefault();
+      event.stopPropagation();
+      if (frm_registro.checkValidity()) 
+      {
+         registrar();
+      }
+      frm_registro.classList.add('was-validated');
+    }, false);
+}
+
+function compararClave(){
+  let correo = document.getElementById("correo");
+  let clave = document.getElementById("clave");
+  event.preventDefault();
+  let usuario = JSON.parse(localStorage.getItem("usuario"));
+  let html;
+  if(correo.value==usuario.correo && clave.value== usuario.clave)
+  {
+    frm_login.classList.remove('was-validated');
+    frm_login.reset();
+    frm_login.classList.add("ocultar");
+    home.classList.remove("ocultar");
+    html = `
+      <nav>
+        <img src="${img_perfil}" />
+        <a href="javascript:void(0);" id="btn_cerrar_sesion" onclick="cerrarSesion();">Cerrar sesion</a>
+      </nav>
+      <h2>Pagina principal </h2> 
+      <br>Hola ${usuario.nombre}
+     `;
+    home.innerHTML = html;
+  }
+  else{
+    alert("Datos incorrectos");
+  }
+}
+
+function registrar(){
+ 
+  let nombre = document.getElementById("nombre");
+  let apellido = document.getElementById("apellido");
+  let celular = document.getElementById("celular");
+  let direccion = document.getElementById("direccion");
+  let correo = document.getElementById("correo1");
+  let clave = document.getElementById("clave1");
+  let usuario = {
+    nombre:nombre.value,
+    apellido:apellido.value,
+    celular:celular.value,
+    direccion:direccion.value,
+    correo:correo.value,
+    clave:clave.value
+  };
+  
+  localStorage.setItem("usuario",JSON.stringify(usuario));
+  alert("¡Muy bien, registro exitoso!")
+  cambiarFormulario();
+}
+
+
