382650921343e1326f55a6dadc2290f14a3d1f84
= <=>
2023-02-25 18:32:41 -0500

    Añadida la pagina home

---
+++ localStorage.js
@@ -0,0 +1,8 @@
+let frm_login, frm_registro, home;
+let btn_enviar_registrar, btn_enviar; 
+let correo; 
+let img_perfil; 
+
+window.onload = function(){
+	frm_login = document.getElementById("frm_login")
+}
